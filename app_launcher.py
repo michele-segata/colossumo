30a151ec0c63f45d2f3e81927bb6fcd6ec19c63e
Michele Segata <segata@ccs-labs.org>
2024-03-06 16:24:51 +0100

    add app_launcher.py that can be used to start an application on an SRN node

---
+++ app_launcher.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+#
+# Copyright (c) 2024 Michele Segata <segata@ccs-labs.org>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with this program.  If not, see http://www.gnu.org/licenses/.
+#
+from argparse import ArgumentParser
+from importlib import import_module
+from logging import basicConfig, DEBUG
+
+
+def main():
+    # set debug level
+    basicConfig(level=DEBUG)
+    parser = ArgumentParser(description="Python script to launch an application on a SRN node")
+    parser.add_argument("--sumoid", help="SUMO vehicle id")
+    parser.add_argument("--colosseumid", help="Colosseum node id", type=int)
+    parser.add_argument("--broker", help="IP address of the broker", default="127.0.0.1")
+    parser.add_argument("--port", help="Port of the broker", default=12345, type=int)
+    parser.add_argument("--params", help="JSON string with simulation params", default="{}")
+    parser.add_argument("--application", help="Python application to launch")
+    args = parser.parse_args()
+    sumo_id = args.sumoid
+    colosseum_id = args.colosseumid
+    broker = args.broker
+    port = args.port
+    module, class_name = args.application.rsplit(".", 1)
+    m = import_module(module)
+    application = getattr(m, class_name)
+    parameters = args.params
+    app = application(sumo_id, broker, port, sumo_id, colosseum_id, parameters, False)
+    app.start_application()
+    app.join_threads()
+
+
+if __name__ == "__main__":
+    main()
