3aa9bd98b94b89fcebbbd5ee04de63526aa4c079
Gabriele Gemmi <g.gemmi@northeastern.edu>
2024-03-12 19:33:57 -0400

    results and scripts

--- analysis/get_results.sh
+++ analysis/get_results.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-# scp mycolosseum-001:/root/OAI-Colosseum/all_logs.pickle ../logs/
-# scp dev-srn-001:~/git/colosseumo/logs/sumo_positions.dat ../logs/
+mkdir ../logs/$1
+scp mycolosseum-001:/root/OAI-Colosseum/all_logs.pickle ../logs/$1
+scp dev-srn-001:~/git/colosseumo/logs/sumo_positions.dat ../logs/$1
 
-scp mycolosseum-002:/root/colosseumo/logs/p.0.log ../logs/
-scp mycolosseum-003:/root/colosseumo/logs/p.1.log ../logs/
-scp mycolosseum-004:/root/colosseumo/logs/p.2.log ../logs/
+scp mycolosseum-002:/root/colosseumo/logs/p.0.log ../logs/$1
+scp mycolosseum-003:/root/colosseumo/logs/p.1.log ../logs/$1
+scp mycolosseum-004:/root/colosseumo/logs/p.2.log ../logs/$1
